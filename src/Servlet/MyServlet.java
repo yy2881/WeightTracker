6544fe03a0a3a047b993ce256d7cfe3e409238ec
yy2881 <45239529+yy2881@users.noreply.github.com>
2020-04-25 18:31:35 -0400

    catch exception

--- src/Servlet/MyServlet.java
+++ src/Servlet/MyServlet.java
@@ -29,52 +29,59 @@ public class MyServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
-		//get input values from html form
-		double currentWeight = Double.parseDouble(request.getParameter("form_curWeight"));
-		double targetWeight = Double.parseDouble(request.getParameter("form_tarWeight"));
-		int activityLevel = Integer.parseInt(request.getParameter("form_activityLevel"));
-		int age = Integer.parseInt(request.getParameter("form_age"));
-		int days = Integer.parseInt(request.getParameter("form_days"));
-		String gender = request.getParameter("form_gender");
-		double height = Double.parseDouble(request.getParameter("form_height")) * 12;
 		
-		
-		
-		//instantiate calculator and calculate nutrition message
-		Calculator calculator = new Calculator();
-		String[] outputMessage = calculator.generateOutput(activityLevel,days,currentWeight,height,targetWeight,age,gender);
+		try {
+			//get input values from html form
+			double currentWeight = Double.parseDouble(request.getParameter("form_curWeight"));
+			double targetWeight = Double.parseDouble(request.getParameter("form_tarWeight"));
+			int activityLevel = Integer.parseInt(request.getParameter("form_activityLevel"));
+			int age = Integer.parseInt(request.getParameter("form_age"));
+			int days = Integer.parseInt(request.getParameter("form_days"));
+			String gender = request.getParameter("form_gender");
+			double height = Double.parseDouble(request.getParameter("form_height")) * 12;
+			
+			
 
-		
-		//instantiate HttpResponseHelper and send http response back
-		HttpResponseHelper helper = new HttpResponseHelper();
-		
-		
-        if(Integer.parseInt(outputMessage[2]) < Integer.parseInt(outputMessage[3])) {
-        	
-        	//handle corner case 1: daily safe calorie intake < daily calorie loss
-        	helper.showReenterPrompt();
-        	
-        	// return response 
-        	response.sendRedirect("index.html");
-        } else { 
-        	// get response writer
-        	PrintWriter writer = response.getWriter();
-        	
-        	//handle corner case 2: daily calorie intake to lose weight is lower than expected healthy value
-        	if(gender.equals("Male") && Integer.parseInt(outputMessage[4]) < 1200 || gender.equals("Female") && Integer.parseInt(outputMessage[4]) < 1000) {
-        		helper.showHealthyWarning();
-        	}
-        	
-        	// build HTML response
-        	String htmlResponse = helper.buildHtmlResponse(outputMessage);
+			//instantiate calculator and calculate nutrition message
+			Calculator calculator = new Calculator();
+			String[] outputMessage = calculator.generateOutput(activityLevel,days,currentWeight,height,targetWeight,age,gender);
 
-            // return response 
-            writer.println(htmlResponse);
-        }
-        	
-        
-        
-        
+			
+			//instantiate HttpResponseHelper and send http response back
+			HttpResponseHelper helper = new HttpResponseHelper();
+			
+			
+	        if(Integer.parseInt(outputMessage[2]) < Integer.parseInt(outputMessage[3])) {
+	        	
+	        	//handle corner case 1: daily safe calorie intake < daily calorie loss
+	        	helper.showReenterPrompt();
+	        	
+	        	// return response 
+	        	response.sendRedirect("index.html");
+	        } else { 
+	        	// get response writer
+	        	PrintWriter writer = response.getWriter();
+	        	
+	        	//handle corner case 2: daily calorie intake to lose weight is lower than expected healthy value
+	        	if(gender.equals("Male") && Integer.parseInt(outputMessage[4]) < 1200 || gender.equals("Female") && Integer.parseInt(outputMessage[4]) < 1000) {
+	        		helper.showHealthyWarning();
+	        	}
+	        	
+	        	// build HTML response
+	        	String htmlResponse = helper.buildHtmlResponse(outputMessage);
+
+	            // return response 
+	            writer.println(htmlResponse);
+	        }
+		} catch (Exception exception) {
+            // Output expected NumberFormatException.
+			JOptionPane.showMessageDialog(null, "Your input is not valid. Please don't leave empty fields and make sure you type number or text as instructed.");
+			response.sendRedirect("index.html");
+        } 
+			
+	        	
+		
+		
 
 	}
 
